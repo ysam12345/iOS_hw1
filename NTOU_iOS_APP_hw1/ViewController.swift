19dcc9ae71da6a304bda9b57735d8572a463c575
yochien <yochien@yochiens-iMac.local>
2019-01-20 11:47:57 +0800

    build the project

---
+++ NTOU_iOS_APP_hw1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NTOU_iOS_APP_hw1
+//
+//  Created by yochien on 2018/10/31.
+//  Copyright © 2018 yochien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NTOU_iOS_APP_hw1Tests/NTOU_iOS_APP_hw1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  NTOU_iOS_APP_hw1Tests.swift
+//  NTOU_iOS_APP_hw1Tests
+//
+//  Created by yochien on 2018/10/31.
+//  Copyright © 2018 yochien. All rights reserved.
+//
+
+import XCTest
+@testable import NTOU_iOS_APP_hw1
+
+class NTOU_iOS_APP_hw1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
